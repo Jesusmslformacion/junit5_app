f07f7eb59b5958ca5e56dd38a5113f96152035dd
Jesusmslformacion <chechuelcrack@gmail.com>
2026-03-02 13:54:56 +0100

    Agregar pruebas unitarias para validar el nombre y saldo de la cuenta en CuentaTest

--- src/test/java/com/jesus/junit5_app/ejemplo/junit5_app/models/CuentaTest.java
+++ src/test/java/com/jesus/junit5_app/ejemplo/junit5_app/models/CuentaTest.java
@@ -1,6 +1,7 @@
 package com.jesus.junit5_app.ejemplo.junit5_app.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.Test;
 
 public class CuentaTest {
 
+    // Test para el nombre de la cuenta
     @Test
     void testNombreCuenta() {
         Cuenta cuenta = new Cuenta("Jesus", new BigDecimal("1000.12345"));
@@ -20,6 +22,15 @@ public class CuentaTest {
          assertEquals(esperado, real);
          assertTrue(real.equals("Jesus"));
 }
+
+    // Test para el saldo de la cuenta
+    @Test
+    void testSaldoCuenta() {
+        Cuenta cuenta = new Cuenta("Jesus", new BigDecimal("1000.12345"));
+        assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
+        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
+        assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
+    }
 
     
 }
