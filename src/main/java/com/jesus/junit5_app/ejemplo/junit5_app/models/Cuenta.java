e979392c1b8b0901fbc7d14cc3ece3bd30e0ee80
Jesusmslformacion <chechuelcrack@gmail.com>
2026-03-02 14:59:34 +0100

    Agregar métodos debito y credito en la clase Cuenta y pruebas unitarias correspondientes en CuentaTest

--- src/main/java/com/jesus/junit5_app/ejemplo/junit5_app/models/Cuenta.java
+++ src/main/java/com/jesus/junit5_app/ejemplo/junit5_app/models/Cuenta.java
@@ -30,6 +30,14 @@ public class Cuenta {
         this.saldo = saldo;
     }
 
+    public void debito(BigDecimal monto) {
+        this.saldo = this.saldo.subtract(monto);
+    }
+
+    public void credito(BigDecimal monto) {
+        this.saldo = this.saldo.add(monto);
+    }
+
     
     // Sobrescribir el método equals para comparar objetos Cuenta
     @Override
--- src/test/java/com/jesus/junit5_app/ejemplo/junit5_app/models/CuentaTest.java
+++ src/test/java/com/jesus/junit5_app/ejemplo/junit5_app/models/CuentaTest.java
@@ -3,6 +3,7 @@ package com.jesus.junit5_app.ejemplo.junit5_app.models;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
@@ -20,6 +21,7 @@ public class CuentaTest {
 
         String esperado = "Jesus";
         String real = cuenta.getPersona();
+        assertNotNull(real);
          assertEquals(esperado, real);
          assertTrue(real.equals("Jesus"));
 }
@@ -28,6 +30,7 @@ public class CuentaTest {
     @Test
     void testSaldoCuenta() {
         Cuenta cuenta = new Cuenta("Jesus", new BigDecimal("1000.12345"));
+        assertNotNull(cuenta.getSaldo());
         assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
         assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
         assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
@@ -44,6 +47,29 @@ public class CuentaTest {
         
         
     }
+
+    // Test para el método debito de la cuenta
+    @Test
+    void testDebitoCuenta() {
+        Cuenta cuenta = new Cuenta("Jesus", new BigDecimal("1000.12345"));
+        cuenta.debito(new BigDecimal(100));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(900, cuenta.getSaldo().intValue());
+        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+        
+    }
+
+    @Test
+    void testCreditoCuenta() {
+        Cuenta cuenta = new Cuenta("Jesus", new BigDecimal("1000.12345"));
+        cuenta.credito(new BigDecimal(100));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(1100, cuenta.getSaldo().intValue());
+        assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
+        
+    }
+
+    
 
     
 
